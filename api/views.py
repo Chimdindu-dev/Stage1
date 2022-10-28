8f61e0c3379c048a65e4741c5a7399a8723379e9
Chimdindu-dev <cchimdindu@gmail.com>
2022-10-28 16:52:35 +0200

    update

--- api/views.py
+++ api/views.py
@@ -4,5 +4,9 @@ from django.http import JsonResponse
 
 @api_view(['GET'])
 def getData(request):
-    person = {"slackUsername":"cchimdindu","backend":True,"age":22,"bio":"i am junior software developer currently taking part in zuri\'s HNG9 program"}
-    return JsonResponse(person)
+    person = JsonResponse({"slackUsername":"cchimdindu","backend":True,"age":22,"bio":"i am junior software developer currently taking part in zuri\'s HNG9 program"})
+    person["access-control-allow-origin"] = "*"
+    person["access-control-allow-methods"] = "GET, OPTIONS"
+    person["access-control-max-age"] = "1000"
+    person["access-control-allow-headers"] = "X-Requested-With, Content-Type"
+    return person
